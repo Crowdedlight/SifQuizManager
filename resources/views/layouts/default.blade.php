35544da6cbd289d7d6c59adde1893b7d72416110
Crowdedlight <crowdedhd@gmail.com>
2017-11-05 02:21:22 +0100

    added autoupdating admission fee shower

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -28,5 +28,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <script src="<?= URL::asset('js/script.js') ?>"></script>
+@yield('scripts')
 </body>
 </html>
--- resources/views/modals/add_team.php
+++ resources/views/modals/add_team.php
@@ -2,7 +2,16 @@
 echo BootForm::open(['route' => ['round.add_team', $round->id]]);
 echo BootForm::hidden('_action', 'add_team');
 echo BootForm::text('TeamName', 'Team Name');
-echo BootForm::number('numPersons', 'Number of Persons in Team');
-?> <small class="text-muted">Enter Team Name and number of persons in the team</small> <?php
+echo BootForm::number('numPersons', 'Number of Persons in Team'); ?>
+    <small class="text-muted">Enter Team Name and number of persons in the team</small>
+    <br/>
+    <hr/>
+    <div class="bs-callout bs-callout-warning" >
+        <label class="control-label">Admission Fee:</label>
+        <br/>
+        <p class="admissionUpdate">0</p>
+    </div>
+    <br/>
+<?php
 echo Button::submit()->withValue('Save')->block();
 echo BootForm::close();
