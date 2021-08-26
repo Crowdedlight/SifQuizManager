e397aa54387dfc480fe90c8aab101b539865f93d
Frederik Mazur Andersen <crowdedhd@gmail.com>
2021-08-27 01:19:11 +0200

    remember to exclude deleted rounds....

--- database/migrations/2021_11_07_221927_increment_total_wins_for_team_per_win.php
+++ database/migrations/2021_11_07_221927_increment_total_wins_for_team_per_win.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use App\Models\Round;
-use App\Models\RoundTeams;
-use App\Models\Team;
 
 class IncrementTotalWinsForTeamPerWin extends Migration
 {
@@ -16,14 +13,19 @@ class IncrementTotalWinsForTeamPerWin extends Migration
     public function up()
     {
         // get all rounds
-        $allRounds = DB::table('rounds')->get();
+        $allRounds = DB::table('rounds')->whereNull('deleted_at')->get();
 
         foreach ($allRounds as $round) {
             // get winning team
-            $winningTeam = DB::table('roundteams')->where('FK_round', $round->id)->where('position', 1)->first();
-            $winningTeamID = $winningTeam->id;
-            $wins = $winningTeam->TotalWins + 1;
-            DB::table('teams')->find($winningTeamID)->update(['TotalWins' => $wins]);
+            $winningTeamID = DB::table('roundteams')->where('FK_round', $round->id)->orderBy('points', 'DESC')->first()->id;
+            $winTeam = DB::table('teams')->where('id', $winningTeamID)->first();
+
+            if (is_null($winTeam)) {
+                continue;
+            }
+
+            $wins = $winTeam->TotalWins + 1;
+            DB::table('teams')->where('id', $winningTeamID)->update(['TotalWins' => $wins]);
         };
     }
 
